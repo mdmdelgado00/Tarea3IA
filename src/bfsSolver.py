380744b520f2991e59cd6112f5d8e2824df72cef
marcelo.delgadomora <marcelo.delgadomora@ucr.ac.cr>
2024-09-17 18:40:53 -0600

    Arreglo a BFS

--- src/bfsSolver.py
+++ src/bfsSolver.py
@@ -6,16 +6,14 @@ def BfsSolver(board):
   queue = deque()
   visited = set()
   queue.append(board)
-  
   # Se convierte a tupla para poder meterlo en el set
   start_tuple = tuple(tuple(row) for row in board)
   visited.add(start_tuple)
-  
   while queue:
     current = queue.popleft()
-    
     # Se convierte a tupla para poder meterlo en el set
     current_tuple = tuple(tuple(row) for row in current)
+    visited.add(current_tuple)
     
     if current == goal:
       print("Goal found")
@@ -23,5 +21,4 @@ def BfsSolver(board):
     for child in get_children(current):
       child_tuple = tuple(tuple(row) for row in child)
       if child_tuple not in visited:
-        visited.add(child_tuple)
         queue.append(child)
