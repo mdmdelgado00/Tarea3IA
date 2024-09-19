8d8b4e899c1584e6b4c2d47bbc4f2e4f1bc4b4c0
JoseM25 <89121494+JoseM25@users.noreply.github.com>
2024-09-19 00:52:56 -0600

    Arreglo final a greedy solver
    Se arregla greedy solver

--- src/greedySolver.py
+++ src/greedySolver.py
@@ -9,26 +9,28 @@ def GreedySolver(board):
     # Set para guardar estados visitados y no repetirlos
     visited = set()
     
-    # Guarda en una lista ordenada de menor a mayor, una tupla de la distancia de Manhattan y el tablero
+    # Guarda en una lista ordenada de menor a mayor, una tupla de el nivel de nodo, la distancia de Manhattan y el tablero
     # Esto evita tener que reordenar la lista cada vez que se agrega un nuevo tablero
-    heapq.heappush(priority_queue, (manhattan_distance(board), board))
+    # Se va a sacar el nodo con el menor nivel, y para nodos con el mismo nivel se sacan basado en su distancia de Manhattan
+    # Esto garantiza un breadth first search y para los nodos del mismo nivel se utiliza la heurística
+    heapq.heappush(priority_queue, (0, manhattan_distance(board), board))
     
     while priority_queue:
-        heuristic_value, current = heapq.heappop(priority_queue)
+        depth, heuristic_value, current = heapq.heappop(priority_queue)
 
         if current == goal:
             print("Goal found")
             return
         
         # Lista de listas a tupla de tuplas para poder utilizar un set
-        start_tuple = tuple(tuple(row) for row in board)
+        current_tuple = tuple(tuple(row) for row in current)
 
-        if start_tuple in visited:
+        if current_tuple in visited:
             continue
         
-        visited.add(start_tuple)
+        visited.add(current_tuple)
         
         for child in get_children(current):
             child_tuple = tuple(tuple(row) for row in child)
             if child_tuple not in visited:
-                heapq.heappush(priority_queue, (manhattan_distance(child), child))
+                heapq.heappush(priority_queue, (depth + 1, manhattan_distance(child), child))
